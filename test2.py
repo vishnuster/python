f35f899e84c61ad31d17ab89648057ce9e0e8935
Vishnu Prakash <vishnu.prakash@morningstar.com>
2019-04-10 13:47:45 +0530

    new cft template yaml file wrongly named test2.py created

---
+++ test2.py
@@ -0,0 +1,30 @@
+Resources:
+ Testinstance:
+   Type: 'AWS::EC2::Instance'
+   Properties:
+     InstanceType: t2.micro
+     ImageId: ami-0080e4c5bc078760e
+     KeyName: cloudformationtest
+     Tags:
+       - Key: Name
+         Value: testinstance
+     SecurityGroups:
+       - !Ref Testsg
+     UserData:
+       'Fn::Base64':
+         |
+           #!/bin/bash -xe
+           useradd vishnu
+           yum install -y httpd
+           echo "<html>This is a test page.</html>" > /var/www/index.html
+           service httpd start
+ Testsg:
+   Type:'AWS::EC2::SecurityGroup'
+   Proeprties:
+     GroupDescription: open port 22 from for machine
+     GroupName: testsg
+     SecurityGroupIngress:
+       - IpProtocol: tcp
+         FromPort: 22
+         ToPort: 22
+         CidrIp: 182.76.82.166/32
